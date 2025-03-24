ccacd9615d81bd9c306e12063fa34aee0261fd5d
Amul Patel <amul@Amuls-MacBook-Pro-2.local>
2025-03-23 23:25:59 -0400

    tail in ll

--- golang/single_linked_list.go
+++ golang/single_linked_list.go
@@ -9,43 +9,45 @@ type Node struct {
 
 type SingleLL struct {
 	head *Node
+	tail *Node
 }
 
+// InsertAtEnd adds a new node at the end
 func (ll *SingleLL) InsertAtEnd(data int) {
-	newNode := Node{data: data, next: nil}
+	newNode := &Node{data: data, next: nil} // Fix: Create a pointer directly
 
 	if ll.head == nil {
-		ll.head = &newNode
+		ll.head = newNode
+		ll.tail = newNode
 		return
 	}
 
-	current := ll.head
-	for current.next != nil {
-		current = current.next
-	}
-
-	current.next = &newNode
+	ll.tail.next = newNode
+	ll.tail = newNode
 }
 
+// InsertAtStart adds a new node at the beginning
 func (ll *SingleLL) InsertAtStart(data int) {
-	newNode := Node{data: data, next: nil}
+	newNode := &Node{data: data, next: ll.head} // Fix: Create a pointer directly
 
-	if ll.head == nil {
-		ll.head = &newNode
-		return
+	ll.head = newNode
+	if ll.tail == nil { // Fix: If list was empty, update tail
+		ll.tail = newNode
 	}
-
-	newNode.next = ll.head
-	ll.head = &newNode
 }
 
+// Delete removes a node with a given value
 func (ll *SingleLL) Delete(data int) {
 	if ll.head == nil {
 		return
 	}
 
+	// If head needs to be deleted
 	if ll.head.data == data {
 		ll.head = ll.head.next
+		if ll.head == nil { // Fix: If list becomes empty, tail should also be nil
+			ll.tail = nil
+		}
 		return
 	}
 
@@ -54,22 +56,26 @@ func (ll *SingleLL) Delete(data int) {
 		current = current.next
 	}
 
-	if current.next != nil {
+	if current.next != nil { // Node found
+		if current.next == ll.tail { // Fix: If deleting the tail node, update tail
+			ll.tail = current
+		}
 		current.next = current.next.next
 	}
 }
 
+// Display prints the linked list
 func (ll *SingleLL) Display() {
-	fmt.Println("\nSingle Linked List")
+	fmt.Println("\nSingle Linked List:")
 	current := ll.head
 	for current != nil {
 		fmt.Printf("%d -> ", current.data)
 		current = current.next
 	}
+	fmt.Println("nil")
 }
 
 func main() {
-
 	ll := SingleLL{}
 	ll.InsertAtEnd(12)
 	ll.InsertAtEnd(13)
@@ -80,8 +86,10 @@ func main() {
 
 	ll.Display()
 
-	ll.Delete(133)
-	ll.Delete(11)
-	ll.Display()
+	ll.Delete(133) // No change, since 133 is not in the list
+	ll.Delete(11)  // Deletes head
+	ll.Delete(20)  // Deletes tail
+	ll.InsertAtEnd(21)
 
+	ll.Display()
 }
